b93f51b7758886f2590f000660ef4e51a44f9cf7
1Pino <Pino.roloff@gmail.com>
2025-11-15 22:33:14 +0100

    Kühlschrankgedichtv1

---
+++ Gedichte/Kuhlschrank/Kuhlschrank.js
@@ -0,0 +1,30 @@
+const divs = document.querySelectorAll('.mydiv');
+
+divs.forEach(div => {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    
+    div.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+    
+    function elementDrag(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        div.style.top = (div.offsetTop - pos2) + 'px';
+        div.style.left = (div.offsetLeft - pos1) + 'px';
+    }
+    
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+});
